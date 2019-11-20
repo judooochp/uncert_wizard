65db375b7053c3f72936e06339b64eab690951e6
judooochp <jakeegill@gmail.com>
2019-11-19 22:27:56 -0600

    Update main.rs
    Get file of choice... if it's in the source file's folder.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
-use std::fs;
+use std::io::*;
+use std::fs::File;
 use uncertWizard::*;
 
 fn main() {
-    let contents = fs::read_to_string("1,2A.ucrt")
-        .expect("Could not read it.");
+    let mut filename = String::new();
+    print!("Let's get the uncertainty of the following file: ");
+    let _=stdout().flush();
+    stdin().read_line(&mut filename).expect("Did not enter nice string.");
+    if let Some('\n')=filename.chars().next_back() {
+        filename.pop();
+    }if let Some('\r')=filename.chars().next_back() {
+        filename.pop();
+    }
+    let contents = get_file(filename).unwrap();
     let new_meas = Measurement::new(contents);
     new_meas.printout();
 }
